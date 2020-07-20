5042b80d7eb795a016f87bbc6f386b4a32249a97
OldaHavelka <67289923+OldaHavelka@users.noreply.github.com>
2020-07-20 05:24:08 +0200

    Create Worker.cs

---
+++ Worker.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Worker : Human, IInterface
+    {
+        int ammountOfLegs;
+        public virtual int AmmountOfLegs { get => this.ammountOfLegs; set { ammountOfLegs=2; }}
+
+        public Worker() {
+            this.ammountOfLegs = 2;
+        }
+
+        public Worker(int ammountOfLegs) {
+            this.ammountOfLegs = ammountOfLegs;
+        }
+        public void payTheEmployee()
+        {
+            Console.WriteLine("Employee has been paid.");
+        }
+
+        public override void TellMeHowManyLegsDoYouHave()
+        {
+            Console.WriteLine("I have {0} legs.", this.ammountOfLegs);
+        }
+    }
+}
